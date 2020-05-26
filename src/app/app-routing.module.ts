dd5f91839ce95048ba6c7219df831d3145072088
Frozen399 <55868488+Frozen399@users.noreply.github.com>
2020-05-26 20:56:43 +0530

    modified contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
   {path:'about',component:AboutComponent},
-  {path:'contact', component:ContactComponent}
 ];
 
 @NgModule({
@@ -14,5 +12,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [AboutComponent,
-                                  ContactComponent];
+export const routingComponents = [AboutComponent];
